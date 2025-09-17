cb37a6baa642e065cad4318186af1dd532156512
Gustavo <gcraneirol@gmail.com>
2025-09-17 14:21:42 -0300

    Criado o contexto do banco de dados e o modelo de aluno

---
+++ back-end/WebApi8-AdmissionTest/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi8_AdmissionTest.Models;
+
+namespace WebApi8_AdmissionTest.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
---
+++ back-end/WebApi8-AdmissionTest/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi8_AdmissionTest.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int Ra { get; set; }
+
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        [MaxLength(150, ErrorMessage = "O Nome deve ter no máximo 150 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "CPF inválido. Use o formato 000.000.000-00.")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        public string Email { get; set; }
+    }
+}
